f8836aacab48da31a0278c91281a3773e954c263
Enciyo <enc1y0@hotmail.com>
2018-10-16 20:48:04 +0300

    RecyclerView-CardView-data model class

--- app/src/main/java/com/example/enciyo/myapplication/HobbiesAdapter.kt
+++ app/src/main/java/com/example/enciyo/myapplication/HobbiesAdapter.kt
@@ -1,10 +1,12 @@
 package com.example.enciyo.myapplication
 
 import android.content.Context
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.enciyo.myapplication.Supplier.hobbies
 import kotlinx.android.synthetic.main.list_item.view.*
 
@@ -29,8 +31,29 @@ class HobbiesAdapter(val context: Context, val hobbies: List<Hobby>): RecyclerVi
 
     inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
 
+        var currentHobby: Hobby? = null
+        var currentPosition: Int=0
+
+        init {
+            itemView.setOnClickListener{
+                Toast.makeText(context,currentHobby!!.title+"\tClicked !",Toast.LENGTH_SHORT).show()
+            }
+            itemView.imgShare.setOnClickListener{
+                val message: String = itemView.txvTitle.text.toString()
+                val intent=Intent()
+                intent.action=Intent.ACTION_SEND
+                intent.putExtra(Intent.EXTRA_TEXT,message)
+                intent.type="text/plain"
+                context.startActivity(intent)
+            }
+
+
+        }
         fun setData(hobby: Hobby?,pos:Int){
             itemView.txvTitle.text=hobby!!.title
+
+            this.currentHobby=hobby
+            this.currentPosition=pos
 
         }
     }
